c63a3b1f1fd82ed9f5c7bc0398177e26b030f982
rand0m <rand0m42195@gmail.com>
2025-07-11 15:30:37 +0800

    add subscribe

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -10,14 +17,19 @@ async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            .route("subscriptions", web::post().to(subscribe))
             .route("/{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
